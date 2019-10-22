7ee04a65fed2193bcf7105d1c3725dfb8bfb2226
AlvinMi <alvin.mi0620@gmail.com>
2019-10-22 23:41:20 +0800

    glslify-loader raw-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,52 @@
 const webpack = require("webpack");
 const path = require("path");
 
+const pathOutput = path.resolve(__dirname, "dist");
+const pathNodeModules = path.resolve(__dirname, "node_modules");
+const env = process.env.NODE_ENV;
+const isProd = env === "production";
+console.log("Environment isProd :", isProd);
+
+const plugins = [new webpack.HotModuleReplacementPlugin()];
+
+if (isProd) {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      compress: {
+        drop_debugger: true,
+        drop_console: true,
+        screw_ie8: true
+      },
+      comments: false,
+      mangle: false
+    })
+  );
+  plugins.push(new ExtractTextPlugin("assets/css/main.css"));
+}
+
+const entry = isProd
+  ? { app: "./src/js/app.js" }
+  : { app: "./src/js/app.js", debug: "./src/js/debug.js" };
+const output = isProd
+  ? {
+      filename: "assets/js/app.js",
+      path: pathOutput
+    }
+  : {
+      filename: "assets/js/[name].js",
+      path: pathOutput
+    };
+const devtool = isProd ? "source-map" : "inline-source-map";
+
 const config = {
-  entry: ["react-hot-loader/patch", "./src/index.js"],
+  entry: ["react-hot-loader/patch", "./src/js/app.js"],
+  // entry,
+  devtool,
+  devServer: {
+    contentBase: "./dist"
+    // host: "0.0.0.0"
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
@@ -13,23 +57,31 @@ const config = {
       {
         test: /\.(js|jsx)$/,
         use: "babel-loader",
-        exclude: /node_modules/
+        exclude: pathNodeModules
       },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(glsl|vert|frag)$/,
+        use: ["raw-loader", "glslify-loader"],
+        exclude: pathNodeModules
       }
     ]
   },
+  // resolve: {
+  //   extensions: [".js", ".jsx"],
+  //   alias: {
+  //     "react-dom": "@hot-loader/react-dom"
+  //   }
+  // }
   resolve: {
-    extensions: [".js", ".jsx"],
     alias: {
-      "react-dom": "@hot-loader/react-dom"
+      libs: path.resolve(__dirname, "src/js/libs"),
+      shaders: path.resolve(__dirname, "src/shaders")
     }
-  },
-  devServer: {
-    contentBase: "./dist"
   }
 };
-
+console.log(config);
 module.exports = config;
